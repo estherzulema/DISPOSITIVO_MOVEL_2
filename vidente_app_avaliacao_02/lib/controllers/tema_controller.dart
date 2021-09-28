7d31b6f3dbd059b666f716f095b805c15a8141fe
Gumercindo-Guilhermina <86901592+Gumercindo-Guilhermina@users.noreply.github.com>
2021-09-27 23:27:33 -0400

    VIDENTE_APP_AVALIAÇÃO_02

--- vidente_app_avaliacao_02/lib/controllers/tema_controller.dart
+++ vidente_app_avaliacao_02/lib/controllers/tema_controller.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class TemaController extends ChangeNotifier {
+  bool usarTemaEscuro;
+  static TemaController instancia = TemaController();
+
+  TemaController() {
+    usarTemaEscuro = false;
+  }
+
+  trocarTema() {
+    usarTemaEscuro = !usarTemaEscuro;
+    notifyListeners();
+  }
+}
